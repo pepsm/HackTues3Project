129ab52f02c537a355dc4f9c99e64399ed926b6b
pepsm <peps.mihailov@gmail.com>
2017-03-25 11:45:37 -0700

    newfunctions

--- main.cpp
+++ main.cpp
@@ -4,22 +4,39 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
-
-using namespace cv;
-using namespace std;
-
-//we will use this colors for The Win form app
+//set of colors
 #define blue  CV_RGB(0,0,255)
 #define green CV_RGB(0,255,0)
 #define red   CV_RGB(255,0,0)
 #define white CV_RGB(255,255,255)
 #define black CV_RGB(0,0,0)
+#define yellow  CV_RGB(230,230, 0)
 
+void display_controls();
+
+//set values for color of the captured object
+int iLowH = 81;
+int iHighH = 123;
+
+int iLowS = 7;
+int iHighS = 165;
+
+int iLowV = 255;
+int iHighV = 255;
+
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	VideoCapture cap(0); //capture the video from webcam
+	//set current color and size
+	CvScalar lineColor = red; 
+    int size_pointer = 6;
 
+	VideoCapture cap(0); //capture the video from webcam
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 400);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 800);
 
 	if (!cap.isOpened())
 	{
@@ -27,37 +44,16 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-	moveWindow("Control", 10, 500);
+    display_controls();
 
-	int iLowH = 81;
-	int iHighH = 123;
-
-	int iLowS = 7;
-	int iHighS = 165;
-
-	int iLowV = 255;
-	int iHighV = 255;
-
-	//Create trackbars in "Control" window
-	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-	createTrackbar("HighH", "Control", &iHighH, 179);
-
-	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-	createTrackbar("HighS", "Control", &iHighS, 255);
-
-	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
-	createTrackbar("HighV", "Control", &iHighV, 255);
-
-	int iLastX = -1;
-	int iLastY = -1;
+	int last_x = -1;
+	int last_y = -1;
 
 	//Capture a temporary image from the camera
 	Mat imgTmp;
 	cap.read(imgTmp);
 
 	//Create a black image with the size as the camera output
-	
 	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);
 	imgLines = Scalar( 0, 0, 0);
 
@@ -68,8 +64,7 @@ int main(int argc, char** argv)
 		Mat imgOriginal;
 
 		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
-
+		
 		if (!bSuccess) //if not success, break loop
 		{
 			cout << "Cannot read a frame from video stream" << endl;
@@ -77,14 +72,12 @@ int main(int argc, char** argv)
 		}
 
 		Mat imgHSV;
-
 		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
 		Mat imgThresholded;
+		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
 
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-
-																									  //morphological opening (removes small objects from the foreground)
+		//morphological opening (removes small objects from the foreground)
 		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
@@ -99,47 +92,49 @@ int main(int argc, char** argv)
 		double dM10 = oMoments.m10;
 		double dArea = oMoments.m00;
 
-		int posX = 0;
-		int posY = 0;
+		int current_x = 0;
+		int current_y = 0;
 		
-
-
 		if (dM10 / dArea >= 0 && dM10 / dArea < 1280 && dM01 / dArea >= 0 && dM01/ dArea < 1280 /* to control the limit */)
 		{
 			//calculate the position of the ball
-			posX = (int)(dM10 / dArea);
-			posY = (int)(dM01 / dArea);
+			current_x = (int)(dM10 / dArea);
+			current_y = (int)(dM01 / dArea);
 		}
 
 		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
 		if (dArea > 10000)
 		{
-			double diff_X = iLastX - posX;
-			double diff_Y = iLastY - posY;
+			double diff_X = last_x - current_x;
+			double diff_Y = last_y - current_y;
 			double magnitude = sqrt(pow(diff_X, 2) + pow(diff_Y, 2));
 
 			// We want to draw a line only if its a valid position
 			//if(iLastX>0 && iLastY>0 && posX>0 && posY>0)
-			if (magnitude > 0 && magnitude < 100 && posX > 120 && posX<530)
+			if (magnitude > 0 && magnitude < 100 && current_x > 120 && current_x <530)
 			{
 				// Draw a line from the previous point to the current point
-				line(imgLines, cvPoint(posX, posY), cvPoint(iLastX, iLastY), red, 6, CV_AA);
+				line(imgLines, cvPoint(current_x, current_y), cvPoint(last_x, last_y), lineColor, size_pointer, CV_AA);
 			}
 			
 
-			iLastX = posX;
-			iLastY = posY;
+			last_x = current_x;
+			last_y = current_y;
 		}
 		
-
 		imshow("Thresholded Image", imgThresholded); //show the thresholded image
 
 		imgOriginal = imgOriginal + imgLines;
 		imshow("Original", imgOriginal); //show the original image
 
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		int key = waitKey(30);
+		if (key == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
 		{
 			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+		
+		else if (key== 115) {
 			//save current frame
 			vector<int> compression_params; //vector that stores the compression parameters of the image
 
@@ -147,23 +142,59 @@ int main(int argc, char** argv)
 
 			compression_params.push_back(98); //specify the compression quality
 											  /* initialize random seed: */
-			srand(time(NULL));
 
-			/* generate secret number between 1 and 10: */
-			int random_n = rand() % 10 - 1;
-			char random = (char)random_n ;
-			bool bSuccess = imwrite(("C:/Users/PC/Desktop/Image" + random + ".jpg"), imgOriginal, compression_params); //write the image to file
+			bool bSuccess = imwrite(("C:/Users/PC/Desktop/Image.jpg"), imgOriginal, compression_params); //write the image to file
 			if (!bSuccess)
 			{
 				cout << "error saving" << endl;
 				waitKey(0);
 			}
-			break;
 		}
-
+		else 
+		{
+			switch (key) {
+			case 98:
+				lineColor = blue;
+				size_pointer = 6;
+				break;
+			case 114:
+				lineColor = red;
+				size_pointer = 6;
+					break;
+			case 101:
+				lineColor = black;
+				size_pointer = 40;
+				break;
+			case 103:
+				lineColor = green;
+				size_pointer = 6;
+				break;
+			case 121:
+				lineColor = yellow;
+				size_pointer = 6;
+				break;
+			
+			}
+		}
 		
 	}
 
 	return 0;
 }
 
+void display_controls() {
+
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+	moveWindow("Control", 10, 100);
+
+	//Create trackbars in "Control" window
+	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", "Control", &iHighH, 179);
+
+	createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", "Control", &iHighS, 255);
+
+	createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+	createTrackbar("HighV", "Control", &iHighV, 255);
+
+}

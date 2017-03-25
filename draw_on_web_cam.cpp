84bd7ece3dd78ad36b9a317e5864612975defc69
pepsm <peps.mihailov@gmail.com>
2017-03-25 05:14:34 -0700

    SavingCurrentFrame

--- draw_on_web_cam.cpp
+++ draw_on_web_cam.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
+#include <stdlib.h>  
+#include <time.h>       
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
+
 using namespace cv;
 using namespace std;
+
+//we will use this colors for The Win form app
+#define blue  CV_RGB(0,0,255)
+#define green CV_RGB(0,255,0)
+#define red   CV_RGB(255,0,0)
+#define white CV_RGB(255,255,255)
+#define black CV_RGB(0,0,0)
+
 
 int main(int argc, char** argv)
 {
 	VideoCapture cap(0); //capture the video from webcam
 
-	if (!cap.isOpened())  // if not success, exit program
+
+	if (!cap.isOpened())
 	{
 		cout << "Cannot open the web cam" << endl;
 		return -1;
 	}
 
 	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+	moveWindow("Control", 10, 500);
 
-	int iLowH = 170;
-	int iHighH = 179;
+	int iLowH = 81;
+	int iHighH = 123;
 
-	int iLowS = 150;
-	int iHighS = 255;
+	int iLowS = 7;
+	int iHighS = 165;
 
-	int iLowV = 60;
+	int iLowV = 255;
 	int iHighV = 255;
 
 	//Create trackbars in "Control" window
@@ -44,15 +57,17 @@ int main(int argc, char** argv)
 	cap.read(imgTmp);
 
 	//Create a black image with the size as the camera output
-	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
+	
+	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);
+	imgLines = Scalar( 0, 0, 0);
 
+	
 
 	while (true)
 	{
 		Mat imgOriginal;
 
 		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
 
 
 		if (!bSuccess) //if not success, break loop
@@ -84,22 +99,38 @@ int main(int argc, char** argv)
 		double dM10 = oMoments.m10;
 		double dArea = oMoments.m00;
 
+		int posX = 0;
+		int posY = 0;
+		
+
+
+		if (dM10 / dArea >= 0 && dM10 / dArea < 1280 && dM01 / dArea >= 0 && dM01/ dArea < 1280 /* to control the limit */)
+		{
+			//calculate the position of the ball
+			posX = (int)(dM10 / dArea);
+			posY = (int)(dM01 / dArea);
+		}
+
 		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
 		if (dArea > 10000)
 		{
-			//calculate the position of the ball
-			int posX = dM10 / dArea;
-			int posY = dM01 / dArea;
+			double diff_X = iLastX - posX;
+			double diff_Y = iLastY - posY;
+			double magnitude = sqrt(pow(diff_X, 2) + pow(diff_Y, 2));
 
-			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+			// We want to draw a line only if its a valid position
+			//if(iLastX>0 && iLastY>0 && posX>0 && posY>0)
+			if (magnitude > 0 && magnitude < 100 && posX > 120 && posX<530)
 			{
-				//Draw a red line from the previous point to the current point
-				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
+				// Draw a line from the previous point to the current point
+				line(imgLines, cvPoint(posX, posY), cvPoint(iLastX, iLastY), red, 6, CV_AA);
 			}
+			
 
 			iLastX = posX;
 			iLastY = posY;
 		}
+		
 
 		imshow("Thresholded Image", imgThresholded); //show the thresholded image
 
@@ -109,9 +140,30 @@ int main(int argc, char** argv)
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
 		{
 			cout << "esc key is pressed by user" << endl;
+			//save current frame
+			vector<int> compression_params; //vector that stores the compression parameters of the image
+
+			compression_params.push_back(CV_IMWRITE_JPEG_QUALITY); //specify the compression technique
+
+			compression_params.push_back(98); //specify the compression quality
+											  /* initialize random seed: */
+			srand(time(NULL));
+
+			/* generate secret number between 1 and 10: */
+			int random_n = rand() % 10 - 1;
+			char random = (char)random_n ;
+			bool bSuccess = imwrite(("C:/Users/PC/Desktop/Image" + random + ".jpg"), imgOriginal, compression_params); //write the image to file
+			if (!bSuccess)
+			{
+				cout << "error saving" << endl;
+				waitKey(0);
+			}
 			break;
 		}
+
+		
 	}
 
 	return 0;
 }
+
